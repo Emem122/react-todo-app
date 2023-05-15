9abc5768183b71567efbde7e42ff3bbdd2054055
Emem122-win <knu.ee1202@gmail.com>
2023-05-15 23:41:51 +0900

    ♻️  refactoring code #11

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,45 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { initialData } from "./data";
 
 function App() {
 	const [input, setInput] = useState("");
-	const [data, setData] = useState([
-		{
-			id: uuidv4(),
-			text: "todo1",
-			isDone: false,
-		},
-		{
-			id: uuidv4(),
-			text: "todo2",
-			isDone: false,
-		},
-		{
-			id: uuidv4(),
-			text: "todo3",
-			isDone: false,
-		},
-		{
-			id: uuidv4(),
-			text: "todo4",
-			isDone: true,
-		},
-	]);
+	const [data, setData] = useState([...initialData]);
 
 	const uncompletedTodo = data.filter((item) => !item.isDone);
 	const completedTodo = data.filter((item) => item.isDone);
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setData([...data, { id: uuidv4(), text: input, isDone: false }]);
+		setInput("");
+	};
+
+	const handleInput = (e) => {
+		e.preventDefault();
+		setInput(e.target.value);
+	};
+
+	const handleComplete = (id) => {
+		const newData = data.map((item) => {
+			if (item.id === id) {
+				return { ...item, isDone: !item.isDone };
+			}
+			return item;
+		});
+		setData(newData);
+	};
+
+	const handleDelete = (id) => {
+		const newData = data.filter((item) => item.id !== id);
+		setData(newData);
+	};
+
 	return (
 		<>
 			<h1>Todo App</h1>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault();
-					setData([...data, { id: uuidv4(), text: input, isDone: false }]);
-					setInput("");
-				}}
-			>
-				<input
-					type="text"
-					placeholder="Add todo..."
-					onChange={(e) => {
-						e.preventDefault();
-						setInput(e.target.value);
-					}}
-					value={input}
-				/>
+			<form onSubmit={handleSubmit}>
+				<input type="text" placeholder="Add todo..." onChange={handleInput} value={input} />
 				<button type="submit">Add</button>
 			</form>
 			<h2>Todo</h2>
@@ -56,20 +48,8 @@ function App() {
 					return (
 						<li key={item.id}>
 							{item.text}
-							<button
-								onClick={() =>
-									setData(
-										data.map((todoItem) =>
-											todoItem.id === item.id ? { ...todoItem, isDone: !todoItem.isDone } : todoItem
-										)
-									)
-								}
-							>
-								Done
-							</button>
-							<button onClick={() => setData(data.filter((todoItem) => todoItem.id !== item.id))}>
-								Delete
-							</button>
+							<button onClick={() => handleComplete(item.id)}>Done</button>
+							<button onClick={() => handleDelete(item.id)}>Delete</button>
 						</li>
 					);
 				})}
@@ -80,20 +60,8 @@ function App() {
 					return (
 						<li key={item.id}>
 							{item.text}
-							<button
-								onClick={() =>
-									setData(
-										data.map((todoItem) =>
-											todoItem.id === item.id ? { ...todoItem, isDone: !todoItem.isDone } : todoItem
-										)
-									)
-								}
-							>
-								Undo
-							</button>
-							<button onClick={() => setData(data.filter((todoItem) => todoItem.id !== item.id))}>
-								Delete
-							</button>
+							<button onClick={() => handleComplete(item.id)}>Undo</button>
+							<button onClick={() => handleDelete(item.id)}>Delete</button>
 						</li>
 					);
 				})}
