d0269aa09fc6fbbd53efe706ab871f106abf1cd9
Emem122-win <knu.ee1202@gmail.com>
2023-05-15 20:34:42 +0900

    create  contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 function App() {
-
-  return (
-    <>
-      <h1>Hello!</h1>
-    </>
-  )
+	return (
+		<>
+			<h1>Todo App</h1>
+			<input type="text" placeholder="Add todo..." />
+			<button>Add</button>
+			<h2>Todo</h2>
+			<ul>
+				<li>
+					todo
+					<button>Done</button>
+					<button>Delete</button>
+				</li>
+				<li>
+					todo
+					<button>Done</button>
+					<button>Delete</button>
+				</li>
+				<li>
+					todo
+					<button>Done</button>
+					<button>Delete</button>
+				</li>
+				<li>
+					todo
+					<button>Done</button>
+					<button>Delete</button>
+				</li>
+			</ul>
+			<h2>Done</h2>
+			<ul>
+				<li>
+					todo
+					<button>Done</button>
+					<button>Delete</button>
+				</li>
+				<li>
+					todo
+					<button>Done</button>
+					<button>Delete</button>
+				</li>
+			</ul>
+		</>
+	);
 }
 
-export default App
+export default App;
